c5a4c7c3bc9dc2352ceb8be13c38188a7cc5d01f
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-08-17 12:38:19 +0600

    Environment Setup for Project:
    Manage configuration settings securely using environment variables with dotenv.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
-
-const port = 5000;
+import config from "./config";
 
 async function catalog() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/book_catalog");
+    await mongoose.connect(config.database_url as string);
     console.log("Server is running! with maker");
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
